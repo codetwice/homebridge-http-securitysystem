e2f7fff26eaca632c30a52d362630a118a0291f9
Demo User <ubuntu@ninja.milkyway>
2016-02-05 13:18:08 +0100

    Added the source files and updated hte README.md.

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # homebridge-http-securitysystem
 Homebridge plugin that creates a SecuritySystem device which uses configurable HTTP calls to set and get its state.
+
+This plugin can be used as a security system in HomeKit/Homebridge. It creates a Homebridge accessory which uses HTTP calls to arm, disarm and check the status of security systems 
+and provides the Service.SecuritySystem service to HomeKit with both the SecuritySystemCurrentState and the SecuritySystemTargetState characteristics implemented.
+
+## Installation
+
+1. Install homebridge using: npm install -g homebridge
+2. Install homebridge-http-securitysystem using: npm install -g homebridge-http-securitysystem
+3. Update your configuration file. See sample-config.json in this repository for a sample. 
+
+## Configuration
+This module requires that the URLs for getting and setting the security system's state are configured correctly. This has to be done in Homebridge's config.json. 
+You can find a sample configuration file in this repository. 
+
+The configuration options are the following:
+
+Configuration expample with explanation
+
+```
+    "accessories": [
+        {
+            "accessory": "Http-SecuritySystem",
+            "name": "Home security",
+            "auth": {	
+                "username": ""
+                "password": "",
+                "immediately": false
+            },
+            "http_method": "POST",
+            "urls": {
+                "stay": { "url": "http://localhost:1880/alarm/arm", "body": "stay" },
+                "away": { "url": "http://localhost:1880/alarm/arm", "body": "away" },
+                "night": { "url": "http://localhost:1880/alarm/arm", "body": "night" },
+                "disarm": { "url": "http://localhost:1880/alarm/disarm", "body": "" },
+                "readCurrentState": { "url": "http://localhost:1880/alarm/check", "body": "" }
+                "readTargetState": { "url": "http://localhost:1880/alarm/check", "body": "" }
+            }
+        }
+    ]
+
+```
+
+The **name** parameter determines the name of the security system you will see in HomeKit.
+The **auth** configuration can be used to specify the username and password if the remote webserver requires HTTP authentication. 
+The **http_method** can be either "GET" or "POST". The HTTP requests going to the target webserver will be using this method.
+The **urls section** configures the URLs that are to be called on certain events. 
+- The **stay**, **away** and **night** URLs are called when HomeKit is instructed to arm the alarm (it has 3 different alarm on states)
+- The **disarm** URL is used when HomeKit is instructed to disarm the alarm
+- The **readCurrentState** and **readTargetState** are used by HomeKit for querying the current state of the alarm device. It should return the following values in the body of the HTTP response:
+  - **"0"**: stay armed
+  - **"1"**: away armed
+  - **"2"**: night armed
+  - **"3"**: disarmed
+  - **"4"**: alarm has been triggered
+
---
+++ index.js
@@ -0,0 +1,149 @@
+var Service, Characteristic;
+var request = require("request");
+
+module.exports = function(homebridge){
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-http-securitysystem", "Http-SecuritySystem", HttpSecuritySystemAccessory);
+}
+
+function HttpSecuritySystemAccessory(log, config) {
+	this.log = log;
+
+	// url info
+	this.urls = {
+		stay: {
+			url: config.urls.stay.url,
+			body: config.urls.stay.body
+		},
+		away: {
+			url: config.urls.away.url,
+			body: config.urls.away.body
+		},
+		night: {
+			url: config.urls.night.url,
+			body: config.urls.night.body
+		},
+		disarm: {
+			url: config.urls.disarm.url,
+			body: config.urls.disarm.body
+		},
+		readCurrentState: {
+			url: config.urls.read.url,
+			body: config.urls.read.body
+		},
+		readTargetState: {
+			url: config.urls.read.url,
+			body: config.urls.read.body
+		}
+	};
+	
+	this.httpMethod = config["http_method"] || "GET";
+	this.auth = {
+		username: config.auth.username,
+		password: config.auth.password,
+		immediately: config.auth.immediately
+	},
+	
+	this.name = config["name"];
+}
+
+HttpSecuritySystemAccessory.prototype = {
+	httpRequest: function(url, body, callback) {
+		request({
+			url: url,
+			body: body,
+			method: this.httpMethod,
+			auth: {
+				user: this.auth.username,
+				pass: this.auth.password,
+				sendImmediately: this.auth.immediately
+			}
+		},
+		function(error, response, body) {
+			callback(error, response, body)
+		})
+	},
+
+	setTargetState: function(state, callback) {
+		this.log("Setting state to %s", state);
+		var cfg = null;
+		
+		switch (state) {
+			case Characteristic.SecuritySystemTargetState.STAY_ARM:
+				cfg = this.urls.stay;
+				break;
+			case Characteristic.SecuritySystemTargetState.AWAY_ARM :
+				cfg = this.urls.away;
+				break;
+			case Characteristic.SecuritySystemTargetState.NIGHT_ARM:
+				cfg = this.urls.night;
+				break;
+			case Characteristic.SecuritySystemTargetState.DISARM:
+				cfg = this.urls.disarm;
+				break;
+		}
+		
+		var url = cfg.url;
+		var body = cfg.body;
+		
+		if (url) {
+			this.httpRequest(url, body, function(error, response, responseBody) {
+				if (error) {
+					this.log('SetState function failed: %s', error.message);
+					callback(error);
+				} else {
+					this.log('SetState function succeeded!');
+					callback(error, response, state);
+				}
+			}.bind(this));
+		} else {
+			callback(null);
+		}
+	},
+	
+	getState: function(url, body, callback) {
+		if (!url) { 
+			callback(null); 
+		}
+		
+		this.httpRequest(url, body, function(error, response, responseBody) {
+			if (error) {
+				this.log('GetState function failed: %s', error.message);
+				callback(error);
+			} else {
+				var state = parseInt(responseBody);
+				this.log("State is currently %s", state);
+				callback(null, state);
+			}
+		}.bind(this));
+	},
+
+	getCurrentState: function(callback) {
+		this.log("Getting current state");
+		this.getState(this.urls.readCurrentState.url, this.urls.readCurrentState.body, callback);
+	},
+	getTargetState: function(callback) {
+		this.log("Getting target state");
+		this.getState(this.urls.readTargetState.url, this.urls.readTargetState.body, callback);
+	},
+	identify: function(callback) {
+		this.log("Identify requested!");
+		callback(); // success
+	},
+
+	getServices: function() {
+		var securityService = new Service.SecuritySystem(this.name);
+
+		securityService
+			.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+			.on('get', this.getCurrentState.bind(this));
+
+		securityService
+			.getCharacteristic(Characteristic.SecuritySystemTargetState)
+			.on('get', this.getTargetState.bind(this))
+			.on('set', this.setTargetState.bind(this));
+
+		return [securityService];
+	}
+};
